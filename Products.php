c796405482447a76e2cd726bd01a335ee375c0c4
Your Name <you@example.com>
2024-10-14 15:54:47 +0200

    N

---
+++ Products.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Cake.css">
+
+    <title>Cake</title>
+</head>
+
+<body>
+
+    <body>
+        <?php 
+        include_once("CommonCode.php");
+        commoncode("Products")
+        ?>
+
+    </body>
+
+</html>
--- commenCode.php
+++ commenCode.php
@@ -1,9 +1,31 @@
 <?php 
-
-
-
-
-
-
-
+function commoncode($PageOpen){}
 ?>
+
+<div class="NavAll">
+            <div class="TopNav">
+                <div class="MainLinks">
+                    <a href="Home.html" <?php if($PageOpen == "Home") {
+                        print("class='active'");
+                    } ?>>Home</a>
+                    <a href="About .html" <?php if($PageOpen == "About") {
+                        print("class='active'");
+                    } ?>>About</a>
+                    <a href="Products.html"<?php if($PageOpen == "Products") {
+                        print("class='active'");
+                    } ?>>Products</a>
+                    <a href="Contact.html" <?php if($PageOpen == "Contact") {
+                        print("class='active'");
+                    } ?>>Contact</a>
+                </div>
+                <div class="Icon">
+                    <img src="./img/download.jpg">
+
+                </div>
+                <a href="#" id="basketIcon"> 🛒 <span id="basketCount"></span>
+            </div>
+        </div>
+
+      <?php 
+      
+      ?>
