074be23ae2fec1eacea3446c7b1aca38b33ff2a9
Your Name <you@example.com>
2024-11-05 15:13:42 +0100

    N

--- ProjectEN/Products.php
+++ ProjectEN/Products.php
@@ -15,7 +15,27 @@
         include_once("CommonCode.php");
         commoncodeNA("Products");
         ?>
+        <div class="AllProducts">
+            <?php
+            $myFile = fopen("pro.csv", "r");
+            $line = fgets($myFile);
+            while (!feof($myFile)) {
+                $line = fgets($myFile);
+                $arrayOfPiesces = explode(";", $line);
+                if (count($arrayOfPiesces) == 5) {
+            ?>
+
+                    <div class="OneProduct">
+                        <div><?= $arrayOfPiesces[1]  ?></div>
+                        <img src="./<?= $arrayOfPiesces[4]  ?>">
+                        <div><?= $arrayOfPiesces[2]  ?>EUR</div>
+                        <div><?= $arrayOfPiesces[3]  ?></div>
+                    </div>
+            <?php
+                }
+            }
+            ?>
+        </div>
 
     </body>
-
 </html>
