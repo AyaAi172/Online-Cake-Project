b1d032b496a2571ec1007918cefb06de35f61a22
Your Name <you@example.com>
2024-11-17 21:34:28 +0100

    N

--- ProjecrFR/ProductsFR.php
+++ ProjecrFR/ProductsFR.php
@@ -14,6 +14,30 @@
     commoncodeNA("ProductsFR");
     ?>
    
+
+   <div class="AllProducts">
+            <?php
+            $myFile = fopen("../ProjectEN/Products details/pro.csv", "r");
+            $line = fgets($myFile);
+            while (!feof($myFile)) {
+                $line = fgets($myFile);
+                $arrayOfPiesces = explode(";", $line);
+                if (count($arrayOfPiesces) == 5) {
+            ?>
+
+                    <div class="OneProduct">
+                        <div class="ProductName"><?= $arrayOfPiesces[1]  ?></div>
+                        <div class="ImageContainer">
+                        <img src="../ProjectEN/Images/<?=$arrayOfPiesces[3]?>" class="ProductImage">
+                        </div>
+                        <div class="Price"><?= $arrayOfPiesces[2] ?> € </div>
+                        <button class="ADD">AJOUTER AU PANIER 🛒</button>
+                    </div>
+            <?php
+                }
+            }
+            ?>
+        </div>
 </body>
 
 </html>
