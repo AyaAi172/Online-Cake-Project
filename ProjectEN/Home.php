e2ff8671128536b3e9c4000d5bf622ad4e6f4125
Your Name <you@example.com>
2024-12-11 12:49:40 +0100

    N

---
+++ ProjectEN/Home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Cake.css?= time() ?>">
+    <title>Cake</title>
+</head>
+
+<body>
+
+    <?php
+    include_once("CommonCode.php");
+    commoncodeNA("Home");
+    $arrayOfStrings["Home"]  
+
+    ?>
+
+</body>
+
+</html>
